f03824683650f396eb07e1146844952143614f51
stylianosgakis <stylianos.gakis98@gmail.com>
2024-08-02 16:23:24 +0200

    Simplify repro project

--- app/src/main/java/com/stylianosgakis/predictive_navigation_repro/MainActivity.kt
+++ app/src/main/java/com/stylianosgakis/predictive_navigation_repro/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionLayout
+import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,7 +17,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,51 +25,53 @@ import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.currentStateAsState
 import androidx.lifecycle.compose.dropUnlessResumed
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
-  @OptIn(ExperimentalSharedTransitionApi::class)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContent {
-      SharedTransitionLayout {
-        CompositionLocalProvider(LocalSharedTransitionScope provides this) {
-          App()
-        }
-      }
+      App()
     }
   }
 }
 
-@Serializable object A
-@Serializable object B
+@Serializable
+object A
 
+@Serializable
+object B
+
+@OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
 private fun App() {
   val navController = rememberNavController()
-  Surface(modifier = Modifier.fillMaxSize()) {
-    NavHost(
-      navController,
-      A::class,
-      modifier = Modifier.fillMaxSize(),
-    ) {
-      navdestination<A> {
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-          Column {
-            Text("A: ${LocalLifecycleOwner.current.lifecycle.currentStateAsState()}")
-            Button({ navController.navigate(B) }) { Text("Go to B") }
-            SharedBox()
+  SharedTransitionLayout {
+    Surface(modifier = Modifier.fillMaxSize()) {
+      NavHost(
+        navController,
+        A::class,
+        modifier = Modifier.fillMaxSize(),
+      ) {
+        composable<A> {
+          Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Column {
+              Text("A: ${LocalLifecycleOwner.current.lifecycle.currentStateAsState()}")
+              Button({ navController.navigate(B) }) { Text("Go to B") }
+              SharedBox( this@composable)
+            }
           }
         }
-      }
-      navdestination<B> {
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-          Column {
-            SharedBox()
-            Text("B: ${LocalLifecycleOwner.current.lifecycle.currentStateAsState()}")
-            Button(dropUnlessResumed { navController.navigate(A) }) { Text("Go to A") }
+        composable<B> {
+          Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Column {
+              SharedBox( this@composable)
+              Text("B: ${LocalLifecycleOwner.current.lifecycle.currentStateAsState()}")
+              Button(dropUnlessResumed { navController.navigate(A) }) { Text("Go to A") }
+            }
           }
         }
       }
@@ -78,13 +81,15 @@ private fun App() {
 
 @OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
-fun SharedBox(modifier: Modifier = Modifier) {
+fun SharedTransitionScope.SharedBox(
+  animatedVisibilityScope: AnimatedVisibilityScope,
+  modifier: Modifier = Modifier,
+) {
   Box(
     modifier
       .sharedElement(
-        LocalSharedTransitionScope.current,
-        LocalNavAnimatedVisibilityScope.current,
         rememberSharedContentState("SharedBox"),
+        animatedVisibilityScope,
       )
       .size(200.dp)
       .background(Color.Red)
